95b30e26221e06d86c4447df5dd06d6b75f9ea4e
Joan Beltran <jsbeltran93@gmail.com>
2022-12-12 11:33:05 -0500

    link in button

--- src/app/modules/rick-morty/components/card-character/card-character.component.ts
+++ src/app/modules/rick-morty/components/card-character/card-character.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MessageService, PrimeIcons } from 'primeng/api';
+
 import { Character } from '../../interface/characters.interface';
 
 @Component({
--- src/app/modules/rick-morty/components/list-item/list-item.component.ts
+++ src/app/modules/rick-morty/components/list-item/list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Character } from '../../interface/characters.interface';
 
@@ -9,4 +10,12 @@ import { Character } from '../../interface/characters.interface';
 })
 export class ListItemComponent {
   @Input() character!: Character;
+
+  constructor(private router: Router) {}
+  goToDetailCharacter(id: number) {
+    // click = 'goToDetailCharacter(character.id)'
+    // [routerLink] =
+    //    "['/detail-character', character.id]";
+    this.router.navigate(['/detail-character', id]);
+  }
 }
